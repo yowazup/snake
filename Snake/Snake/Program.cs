3afbac9dc238c3bff7d29c3702d9cb9c5de222a2
Nikita <nikita.zakharov@gmail.com>
2022-09-23 18:00:30 +0300

    list

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,6 +14,35 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            Point p3 = new Point(3, 2, '?');
+            p3.Draw();
+
+            Point p4 = new Point(0, 8, '@');
+            p4.Draw();
+
+            List<char> numlist = new List<char>();
+            numlist.Add('#');
+            numlist.Add('*');
+            numlist.Add('!');
+
+            char x = numlist[0];
+            char y = numlist[1];
+            char z = numlist[2];
+            
+
+            foreach (char i in numlist)
+            {
+                Console.WriteLine(i);
+            }
+
+            numlist.RemoveAt(0);
+
+            List<Point> plist = new List<Point>();
+            plist.Add(p1);
+            plist.Add(p2);
+            plist.Add(p3);
+            plist.Add(p4);
+
             Console.ReadLine();
         }
 
