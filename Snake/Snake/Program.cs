dae6481425d7e7c4fecc3ae33ab1bf5c76ceafa3
Nikita <nikita.zakharov@gmail.com>
2022-09-23 16:46:36 +0300

    classes

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Snake;
 using System;
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -6,29 +7,22 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
 
-            Draw(x1, y1, sym1);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
 
             Console.ReadLine();
         }
 
-        static void Draw (int x, int y, char sym)
-        {
-
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-
-
-        }
 
     }
 }
